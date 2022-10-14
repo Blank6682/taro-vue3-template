40bb74585f319bb8d89981d03d2b5e4e19378b39
XiQuan Wu <2452402628@qq.com>
2022-10-14 22:20:58 +0800

    feat : auto import el

---
+++ src/components/MyComponents.vue
@@ -0,0 +1,16 @@
+<template>
+  <div center>
+    {{msg}}
+  </div>
+</template>
+
+<script setup lang="ts">
+const { msg } = defineProps<{
+  msg: string
+}>()
+
+</script>
+
+<style scoped>
+
+</style>
